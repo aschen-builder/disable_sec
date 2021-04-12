1c8de0ff8fa8b421ac0e5e1f455e42c3df54ddc9
Austin Chick <auschick.work@gmail.com>
2021-04-12 10:03:48 -0700

    added spotify

--- ps1/run.ps1
+++ ps1/run.ps1
@@ -12,3 +12,4 @@ adb shell pm disable-user com.sec.android.app.wallpaperchooser
 adb shell pm disable-user com.sec.android.gallery3d
 adb shell pm disable-user com.sec.android.mimage.photoretouching
 adb shell pm disable-user com.sec.android.app.clockpackage
+adb shell pm disable-user com.spotify.music
