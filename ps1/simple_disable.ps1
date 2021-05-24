50c8005827ba64746a283e25fe174a7a95cb3401
Maddie Thomas <7356701+madelynthomas@users.noreply.github.com>
2021-05-24 13:37:13 -0700

    Added: Simple disable file for Windows 10

---
+++ ps1/simple_disable.ps1
@@ -0,0 +1,18 @@
+# For Windows; place this file in the same directory as adb.exe
+.\adb.exe devices
+
+.\adb.exe shell pm disable-user com.google.android.youtube
+.\adb.exe shell pm disable-user com.google.android.googlequicksearchbox
+.\adb.exe shell pm disable-user com.google.android.apps.maps
+.\adb.exe shell pm disable-user com.google.android.gm
+.\adb.exe shell pm disable-user com.google.android.videos
+.\adb.exe shell pm disable-user com.android.chrome
+.\adb.exe shell pm disable-user com.google.android.music
+.\adb.exe shell pm disable-user com.google.android.apps.docs
+.\adb.exe shell pm disable-user com.sec.spp.push
+.\adb.exe shell pm disable-user com.sec.android.app.myfiles
+.\adb.exe shell pm disable-user com.sec.android.app.wallpaperchooser
+.\adb.exe shell pm disable-user com.sec.android.gallery3d
+.\adb.exe shell pm disable-user com.sec.android.mimage.photoretouching
+.\adb.exe shell pm disable-user com.sec.android.app.clockpackage
+.\adb.exe shell pm disable-user com.spotify.music
